4fa778cc8b2ec90b34a80bf5c08dbb5c3a941ab1
cesar <haliercesr@gmail.com>
2024-10-12 20:14:30 -0300

    estructura de Pilas

--- src/com/aluracursos/estructuradedatos/modelos/Pila.java
+++ src/com/aluracursos/estructuradedatos/modelos/Pila.java
@@ -8,14 +8,20 @@ public class Pila {
     private List<String> nombres = new LinkedList<String>();
 
         public void insertar(String nombre) {
-
+            nombres.add(nombre);
         }
 
         public String remove() {
-            return "";
+            return nombres.remove(nombres.size()-1); //usamos la funcion de linkedlist
         }
 
         public boolean vacia() {
-            return false;
+            return nombres.size() == 0;
+            //return nombres.isEmpty(); //segundo metodo
         }
+
+    @Override
+    public String toString() {
+        return nombres.toString();
+    }
 }
--- src/com/aluracursos/estructuradedatos/principal/TestDeLaPila.java
+++ src/com/aluracursos/estructuradedatos/principal/TestDeLaPila.java
@@ -1,5 +1,39 @@
 package com.aluracursos.estructuradedatos.principal;
 
-public class TestDeLaPila {
+import com.aluracursos.estructuradedatos.modelos.Pila;
 
+import java.util.Stack;
+
+public class TestDeLaPila {
+    public static void main(String[] args) {
+        //En las pilas el primer elemento en entrar es el último en salir
+        Pila pila = new Pila();
+
+        System.out.println(pila.vacia());
+        pila.insertar("Mauricio");
+        System.out.println(pila);
+        pila.insertar("Guilherme");
+        System.out.println(pila);
+        System.out.println(pila.vacia());
+        String r1 = pila.remove();
+        System.out.println(r1);
+
+        String r2 = pila.remove();
+        System.out.println(r2);
+
+        System.out.println(pila);
+        System.out.println(pila.vacia());
+
+        Stack<String> stack = new Stack<String>(); //es la clase de JAVA para las pilas con sus propios metodos pop y push
+        stack.push("Mauricio");
+        stack.push("Marcelo");
+
+        System.out.println(stack);
+        stack.pop(); //elimina el ultimo elemento de la pila
+        System.out.println(stack);
+
+        String nombre = stack.peek(); //devuelve el ultimo elemento sin eliminarlo
+        System.out.println(nombre);
+
+    }
 }
